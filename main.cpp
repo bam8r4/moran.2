751d307e64f4c845cd92dc86341d7e5e734f3111
Brent Moran <brentmoran@Brents-MacBook-Pro-2.local>
2020-10-06 22:13:24 -0500

    Added some logic to allow forking.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include <stdio.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -6,7 +8,37 @@ int main()
 {
 	cout << "Hello world" << endl;
 
-	return 0;
+	int counter = 0;
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        // child process
+        int i = 0;
+        for (; i < 5; ++i)
+        {
+            printf("child process: counter=%d\n", ++counter);
+        }
+    }
+    else if (pid > 0)
+    {
+        // parent process
+        int j = 0;
+        for (; j < 5; ++j)
+        {
+            printf("parent process: counter=%d\n", ++counter);
+        }
+    }
+    else
+    {
+        // fork failed
+        printf("fork() failed!\n");
+        return 1;
+    }
+
+    printf("--end of program--\n");
+
+    return 0;
 
 
 }
