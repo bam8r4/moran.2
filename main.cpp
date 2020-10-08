35b9afa4feb7f56a49a1442d6463d1e42c72bbe9
Brent Moran <brentmoran@Brents-MacBook-Pro-2.local>
2020-10-07 20:57:08 -0500

    Added command line parsing as well as defaults needed for the project.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
-#include<iostream>
+#include <iostream>
 #include <stdio.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
+
+ int maxNumChildren = 4;
+ int concurrentChildren = 2;
+ int maxTimeSeconds = 100;
+
+ int option_index = 0;
+ char *user_name = NULL;
+ while (( option_index = getopt(argc, argv, "n:s:t:h")) != -1){
+   switch (option_index) {
+     case 'n':
+       maxNumChildren = atoi(optarg);
+       break;
+     case 's':
+       concurrentChildren = atoi(optarg);
+       break;
+		 case 't':
+	     maxTimeSeconds = atoi(optarg);
+	     break;
+		 case 'h':
+	 		 printf("Printing usage:\n");
+			 return 0;
+     default:
+      printf("Incorrect options.\n");
+      return 1;
+     } //end block for switch
+   }  //end block for while
+
+	cout<<"Max num childrens "<<maxNumChildren<<endl;
+	cout<<"Max concurrent "<<concurrentChildren<<endl;
+	cout<<"Max time "<<maxTimeSeconds<<endl;
 
 	char *argv[] = {"racecar", NULL };;
 	cout << "Hello world" << endl;
